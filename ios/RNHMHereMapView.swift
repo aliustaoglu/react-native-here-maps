e389987a6f3f6467eb8f767884e738d19a2d3d1f
Cuneyt Aliustaoglu <aliustaoglu@yahoo.com>
2020-03-16 08:57:29 +1300

    iOS - RNHMHereMapView base class for Map

---
+++ ios/RNHMHereMapView.swift
@@ -0,0 +1,9 @@
+//
+//  RNHMHereMapView.swift
+//  RNHMReactNativeHereMaps
+//
+//  Created by Cuneyt Aliustaoglu on 16/03/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
